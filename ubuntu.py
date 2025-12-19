a10a92317cfa8fcf07a0624e454cb0b0c6eb83db
alexandrbabatenko-cmyk <alexandr.babatenko@gmail.com>
2025-12-19 21:29:55 +0300

    Update ubuntu.py

--- ubuntu.py
+++ ubuntu.py
@@ -189,5 +189,6 @@ document.getElementById('exchangeBtn').onclick = async () => {
 """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000"))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
